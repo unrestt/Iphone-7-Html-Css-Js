1512a41be4bfc6063610e0a73ebbde385ca74a6d
Bartosz Kutnik <kutnik.contact@gmail.com>
2024-01-14 14:46:01 +0100

    Delete PhoneImitationProject directory

--- PhoneImitationProject/assets/js/message.js
+++
@@ -1,7 +0,0 @@
-function appendToInput(value) {
-    document.getElementById('textInput').value += value;
-  }
-
-  function clearInput() {
-    document.getElementById('textInput').value = '';
-  }
